db62622bf5518264cdc9c1a3edd9ec188da0d22b
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-09-12 15:17:45 -0500

    added necessary node module requirements

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,23 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const generateMD = require("./utils/generateMarkdown.js")
+
+const writeFileAsync = util.promisify(fs.writeFile);
+// array of questions for user
+function questions() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your application?"
+        },
+        {
+            type: ""
+        }
+    ])
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
