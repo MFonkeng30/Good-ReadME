529be168f3c32395f2adcac4dfe7809fd77b5c47
Muendem Fonkeng <fonkengmuendem@yahoo.com>
2020-09-15 19:33:34 -0500

    added code to render the generated README

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,36 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Decription
+  ${data.description}
+
+  ## Table of Contents
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](#usage)
+  [License](#license)
+  [Contributors](#contributors)
+  [Tests](#tests)
+  [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions
+  If you have any questions, please reach out to me at ${data.github},
+  or via email at ${data.email}
 `;
 }
 
